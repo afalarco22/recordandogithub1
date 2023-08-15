9d42142bb085746b625cf9cff3b7e5f5050b7b05
309 <309@academicos.cesde.edu.co>
2023-08-15 08:06:14 -0500

    feat:creamos variables primitivas y lita de datos

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+nombreUsuario = "Andrew"
+edadUsuario = 34
+estaturaUsuario = 1.78
+esHinchaDelVerde = True
+
+
+
+print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años")
+
+comidasFavoritas =["frisoles", "Sushicharrones", "mazamorra", "Carne Molida"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
